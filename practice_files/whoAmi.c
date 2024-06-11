d6a339f094b92ea555b6130aad4f4854ea6a7616
solarianvulpine <victoria.teale222@gmail.com>
2024-06-11 17:04:06 +0000

    created the whoami file which will determine if a number is even or odd

--- practice_files/whoAmi.c
+++ practice_files/whoAmi.c
@@ -1,1 +1,19 @@
-#include <
+#include <stdio.h>
+
+char main( int whoAmI)
+{
+    int num;
+    int Result;
+
+    printf("Please enter a number: ");
+    scanf(" %d", &num);
+
+    if (num % 2 == 0)
+    {
+        printf("%d is even\n", num);
+    }
+    else
+    {
+        printf("%d is odd\n", num);
+    }
+}
